9d90363c9b32a9a84d752ccef9b579b3445cd29c
helenasoaresp <helenasoarees23@gmail.com>
2026-03-06 10:48:06 -0300

    feat: adicionar model Autor e suas  views e serializers

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,11 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 
-from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet
 
 router = DefaultRouter()
 
+router.register(r'autores', AutorViewSet, basename='autores')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'editoras', EditoraViewSet, basename='editoras')
 router.register(r'users', UserViewSet, basename='users')
--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,3 +2,4 @@ from click.core import F
 from .user import UserSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
+from .autor import AutorSerializer
